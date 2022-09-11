e7cf7446dc4eb32711cc5958810a4074c52a2b49
sajad <sjdsaj63@gmail.com>
2022-09-11 14:20:56 +0530

    reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl,FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactiveForm';
+
+  constructor(private fb : FormBuilder){}
+  // creating a new FormGroup
+  variable = this.fb.group(
+    {
+      email : ['',[Validators.required,Validators.pattern("^[a-z0-9.%]+@[a-z0-9.-]+\.[a-z]{2,4}")]],
+      pswd : ['',[Validators.required,Validators.minLength(8),Validators.pattern('^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$')]]
+    }
+  )
+
+
+  check(){
+    console.log(this.variable.controls.email.status)
+  }
 }
